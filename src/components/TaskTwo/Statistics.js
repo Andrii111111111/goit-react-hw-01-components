0ab6f7c24e2b085789023964260970f5e8e01644
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-10-01 14:56:03 +0200

    1

--- src/components/TaskTwo/Statistics.js
+++ src/components/TaskTwo/Statistics.js
@@ -18,10 +18,10 @@ export const TaskTwo = () => {
   );
 };
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-console.log(styles.item);
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// console.log(styles.item);
 // styles.item.style.backgroundColor = getRandomHexColor();
