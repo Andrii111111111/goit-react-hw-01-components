c5b5fdfae48fb22d003d09c69728c11503542aa8
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-10-03 00:02:08 +0200

    Update Statistics.js

--- src/components/TaskTwo/Statistics.js
+++ src/components/TaskTwo/Statistics.js
@@ -1,8 +1,6 @@
 import data from '../../data/data.json';
 import styles from './Statistics.module.css';
-import clsx from 'clsx';
 
-let li = document.querySelectorAll('#qwe');
 export const TaskTwo = () => {
   return (
     <section className={styles.statistics}>
@@ -35,8 +33,8 @@ export const TaskTwo = () => {
 //   li.style.backgroundColor = getRandomHexColor();
 // }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
